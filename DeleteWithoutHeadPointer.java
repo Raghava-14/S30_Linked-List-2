c681b9b0c4d0bbd14019686e0baba4cf23ed36de
Raghava-14 <73310160+Raghava-14@users.noreply.github.com>
2023-04-18 23:41:10 -0500

    Create DeleteWithoutHeadPointer.java

---
+++ DeleteWithoutHeadPointer.java
@@ -0,0 +1,33 @@
+//Time = O(1)
+//Space = O(1)
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+class Solution
+{
+    void deleteNode(Node del)
+    {
+        // If given node is null, just return
+        if(del == null) return;
+        
+        // If given node is the last node, set it to null and return
+        if(del.next == null) {
+            del = null;
+            return;
+        }
+        
+        // Copy data of next node to given node
+        Node nextNode = del.next;
+        del.data = nextNode.data;
+        
+        // Update the pointers to skip the next node
+        del.next = nextNode.next;
+        nextNode = null;
+    }
+}
+
